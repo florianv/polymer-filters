5572ac99f5631db2d565c4d16a7ad0c2fb387650
florianv <voutzinos.florian@gmail.com>
2014-08-30 23:29:29 +0200

    Added abs filter

---
+++ filter-abs.js
@@ -0,0 +1,8 @@
+/**
+ * Return the absolute value of a number
+ * @param  {number} number
+ * @return {number} absolute value
+ */
+PolymerExpressions.prototype.abs = function (number) {
+    return Math.abs(number);
+};
